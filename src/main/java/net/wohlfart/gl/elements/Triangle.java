2e309f2e782491ee734036d928bd33c0bee8c732
michael <michael@wohlfart.net>
2012-12-08 10:05:34 +0100

    simpler triangle

--- src/main/java/net/wohlfart/gl/elements/Triangle.java
+++ src/main/java/net/wohlfart/gl/elements/Triangle.java
@@ -24,15 +24,15 @@ public class Triangle implements IDrawable, IPoolable {
 		}
 	};
 
+	// A VAO can have up to 16 attributes (VBO's) assigned to it by default
 	private int vaoId;   // Vertex Array Object  
 	private int vboId;   // Our Vertex Buffer Object 
 
 	private int vertexCount;
-	private int indicesCount;
 
 
 	/**
-	 * only callec created by the pool
+	 * only called by the pool
 	 */
 	private Triangle() {}
 
@@ -55,42 +55,32 @@ public class Triangle implements IDrawable, IPoolable {
 				bl.x, bl.y, bl.z,
 				br.x, br.y, br.z};
 		result.vertexCount = 3;
-		
-		byte[] indices = {0, 1, 2};
-		result.indicesCount = indices.length;
 
-
-		// Create a new Vertex Array Object in memory and select it (bind)
-		// A VAO can have up to 16 attributes (VBO's) assigned to it by default
+		// create a  VAO in memory
 		result.vaoId = GL30.glGenVertexArrays();
+		// select/bind the VAO
 		GL30.glBindVertexArray(result.vaoId);
-
-		// Sending data to OpenGL requires the usage of (flipped) byte buffers
+		
+		// create a VBO in the VAO
+		result.vboId = GL15.glGenBuffers();
+		// select/bin the VBO
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, result.vboId);
+		
+		// create & flip a buffer for the vertices
 		FloatBuffer verticesBuffer = BufferUtils.createFloatBuffer(result.vertexCount * 3);
 		verticesBuffer.put(vertices);
 		verticesBuffer.flip();
+		
+		// set the size and data of the VBO and set it to STATIC_DRAW
+		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, verticesBuffer, GL15.GL_STATIC_DRAW);
 
-		ByteBuffer indicesBuffer = BufferUtils.createByteBuffer(result.indicesCount);
-		indicesBuffer.put(indices);
-		indicesBuffer.flip();
-
-		// Create a new VBO for the indices and select it (bind)
-		result.vboId = GL15.glGenBuffers();
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, result.vboId);
-		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, verticesBuffer, GL15.GL_STATIC_DRAW);		// Deselect (bind to 0) the VBO
-		// Put the VBO in the attributes list at index 0
+		// set up our vertex attributes pointer 
 		GL20.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 0, 0);
-		// Deselect (bind to 0) the VBO
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
 
-		// Create a new VBO for the indices and select it (bind)
-		result.vboId = GL15.glGenBuffers();
-		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, result.vboId);
-		GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, indicesBuffer, GL15.GL_STATIC_DRAW);
-		// Deselect (bind to 0) the VBO
-		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
-
-		// Deselect (bind to 0) the VAO
+		// disable VAO
+		GL20.glEnableVertexAttribArray(0);
+		
+		// disable VBO
 		GL30.glBindVertexArray(0);
 
 		return result;
@@ -116,10 +106,6 @@ public class Triangle implements IDrawable, IPoolable {
 		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
 		GL15.glDeleteBuffers(vboId);
 
-		// Delete the index VBO
-		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
-		GL15.glDeleteBuffers(vboId);
-
 		// Delete the VAO
 		GL30.glBindVertexArray(0);
 		GL30.glDeleteVertexArrays(vaoId);
@@ -129,22 +115,11 @@ public class Triangle implements IDrawable, IPoolable {
 
 	@Override
 	public void draw() {
-		// Bind to the VAO that has all the information about the vertices
+		// bind to the VAO
 		GL30.glBindVertexArray(vaoId);
-		GL20.glEnableVertexAttribArray(0);
-
-		// Bind to the index VBO that has all the information about the order of the vertices
-		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vboId);
-
-		// Draw the vertices
+		// draw the vertices
 		GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, vertexCount);
-
-		// Draw the vertices
-		GL11.glDrawElements(GL11.GL_TRIANGLES, indicesCount, GL11.GL_UNSIGNED_BYTE, 0);
-
-		// Put everything back to default (deselect)
-		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
-		GL20.glDisableVertexAttribArray(0);
+		// unbind the VAO
 		GL30.glBindVertexArray(0);
 	}
 
