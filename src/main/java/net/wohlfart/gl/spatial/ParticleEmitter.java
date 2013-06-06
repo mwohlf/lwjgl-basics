d404804ca55ddbf71dfc94bfa7104636a652c1bf
michael <michael@wohlfart.net>
2013-06-06 02:38:43 +0200

    fix fileloading

--- src/main/java/net/wohlfart/gl/spatial/ParticleEmitter.java
+++ src/main/java/net/wohlfart/gl/spatial/ParticleEmitter.java
@@ -1,5 +1,9 @@
 package net.wohlfart.gl.spatial;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 import java.util.HashSet;
 import java.util.Set;
@@ -7,10 +11,15 @@ import java.util.Set;
 import net.wohlfart.gl.elements.AbstractRenderable;
 import net.wohlfart.gl.renderer.IsRenderable;
 import net.wohlfart.gl.shader.mesh.ParticleMesh;
+import net.wohlfart.tools.PNGDecoder;
+import net.wohlfart.tools.PNGDecoder.Format;
 import net.wohlfart.tools.SimpleMath;
 import net.wohlfart.tools.Vertex;
 
 import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL13;
+import org.lwjgl.opengl.GL30;
 import org.lwjgl.util.vector.Vector3f;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,6 +30,7 @@ public class ParticleEmitter extends AbstractRenderable implements Emitter {
     Set<Particle> particles = new HashSet<Particle>();
     int newPerSecond; // new particles per second;
     float leftover; // fract from last update
+    Integer commonTextureId;
 
     public ParticleEmitter() {
         this.newPerSecond = 2;
@@ -40,11 +50,15 @@ public class ParticleEmitter extends AbstractRenderable implements Emitter {
     @Override
     protected IsRenderable setupMesh() {
         LOGGER.info("rendering {} particles", particles.size());
+        if (commonTextureId == null) {
+            commonTextureId  = loadPNGTexture("/gfx/images/ash_uvgrid01.png", GL13.GL_TEXTURE0);
+        }
 
         final ParticleMesh.Builder builder = new ParticleMesh.Builder();
         builder.setFlippedFloatBuffer(getFlippedFloatBuffer());
         builder.setParticleCount(particles.size());
-        builder.setTextureFilename("/gfx/images/ash_uvgrid01.png");
+        builder.setTextureId(commonTextureId);
+        // builder.setTextureFilename("/gfx/images/ash_uvgrid01.png");
         return builder.build();
     }
 
@@ -80,5 +94,45 @@ public class ParticleEmitter extends AbstractRenderable implements Emitter {
         return verticesBuffer;
     }
 
+
+    private int loadPNGTexture(String filename, int textureUnit) {
+        int texId = 0;
+
+        // InputStream inputStream = new FileInputStream(filename);
+        try (InputStream inputStream = ClassLoader.class.getResourceAsStream(filename)) {
+
+            // Link the PNG decoder to this stream
+            final PNGDecoder decoder = new PNGDecoder(inputStream);
+            // Get the width and height of the texture
+            final int tWidth = decoder.getWidth();
+            final int tHeight = decoder.getHeight();
+            // Decode the PNG file in a ByteBuffer
+            final ByteBuffer buffer = ByteBuffer.allocateDirect(4 * decoder.getWidth() * decoder.getHeight());
+            decoder.decode(buffer, decoder.getWidth() * 4, Format.RGBA);
+            buffer.flip();
+
+            // Create a new texture object in memory and bind it
+            texId = GL11.glGenTextures();
+            GL13.glActiveTexture(textureUnit);
+            GL11.glBindTexture(GL11.GL_TEXTURE_2D, texId);
+            // All RGB bytes are aligned to each other and each component is 1 byte
+            GL11.glPixelStorei(GL11.GL_UNPACK_ALIGNMENT, 1);
+            // Upload the texture data and generate mip maps (for scaling)
+            GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA8, tWidth, tHeight, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buffer);
+            GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
+            // Setup the ST coordinate system
+            GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL11.GL_REPEAT);
+            GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL11.GL_REPEAT);
+            // Setup what to do when the texture has to be scaled
+            GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
+            GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR_MIPMAP_LINEAR);
+
+        } catch (final FileNotFoundException ex) {
+            LOGGER.error("can't load texture image", ex);
+        } catch (final IOException ex) {
+            LOGGER.error("can't load texture image", ex);
+        }
+        return texId;
+    }
 
 }
