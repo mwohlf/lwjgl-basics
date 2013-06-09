fff6909f903aa35e88c2741f0c223e4778953c0f
michael <michael@wohlfart.net>
2013-06-09 14:56:46 +0200

    minor shader changes

--- src/main/resources/shaders/lighting/fragment.glsl
+++ src/main/resources/shaders/lighting/fragment.glsl
@@ -1,4 +1,6 @@
-#version 330 compatibility
+#version 330 core
+
+// fragments are possible pixels
 
 uniform sampler2D texture01;
 uniform sampler2D texture02;
@@ -9,12 +11,14 @@ in vec3 pass_Normal;          // must be already normalized
 in vec3 pass_Position;
 in vec3 pass_LightPosition;
 
+out vec4 gl_FragColor;
 
 void main(void) {
     vec4 lightColor = vec4 (1, 1, 1, 0.0);
 
     vec3 lightRay = normalize(pass_LightPosition - pass_Position);
 
+    // the fragment shaders job is to generate a gl_FragColor value as a vec4()
     gl_FragColor = texture2D(texture01, pass_TextureCoord)
             * max(dot(pass_Normal, lightRay), 0.1)
             * lightColor;
--- src/main/resources/shaders/lighting/vertex.glsl
+++ src/main/resources/shaders/lighting/vertex.glsl
@@ -1,9 +1,13 @@
 #version 330 core
 
+//
+//  see: http://pyopengl.sourceforge.net/context/tutorials/shader_6.xhtml
+//
+
 layout (location = 0) in vec3 in_Position;
 layout (location = 1) in vec3 in_Normal;
 layout (location = 2) in vec2 in_TexCoord;
-layout (location = 3) in vec4 in_Color;   //RGB alpha
+layout (location = 3) in vec4 in_Color;      // RGBA
 
 uniform mat4 modelToWorldMatrix;     // modelMatrix
 uniform mat4 worldToCameraMatrix;    // viewMatrix
@@ -20,19 +24,21 @@ out vec3 pass_LightPosition;
 
 void main(void) {
 
-    // appy the inverted&transposed of the model matrix to make up for model rotation
+    // apply the inverted & transposed of the model matrix to make up for model rotation
     pass_Normal = normalize(normalMatrix * in_Normal);
 
     // we do nothing to the color and texture here
     pass_Color = in_Color;
     pass_TextureCoord = in_TexCoord;
 
-    // light must already be in worldspace, shoudl be the same for all models
+    // light must already be in worldspace, should be the same for all models
     pass_LightPosition = vec3(worldToCameraMatrix * vec4(lightPosition,0));
 
     //
     pass_Position = vec3(worldToCameraMatrix * modelToWorldMatrix * vec4(in_Position, 1.0));
 
+
+    // generate a gl_Position value, which must be a vec4()
     gl_Position = cameraToClipMatrix
                   * worldToCameraMatrix
                   * modelToWorldMatrix
